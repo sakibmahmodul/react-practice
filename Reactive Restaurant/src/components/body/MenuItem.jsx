af7701384d6a7b01edd41e992eecc91c68d26b67
sakibmahmodul <sakibuddessho89@gmail.com>
2025-04-09 19:36:26 +0600

    Menu Item

---
+++ Reactive Restaurant/src/components/body/MenuItem.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Menu from './Menu';
+
+const MenuItem = props => {
+    console.log(props);
+    return(
+        <div>
+
+        </div>
+    )
+}
+export default MenuItem;
