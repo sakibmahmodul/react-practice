2f1d3211ed1c9ac34d7a6ec03b8261e1d49a269b
sakibmahmodul <sakibuddessho89@gmail.com>
2025-04-09 14:56:13 +0600

    Component Tree

--- Reactive Restaurant/src/App.jsx
+++ Reactive Restaurant/src/App.jsx
@@ -1,14 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from 'react'
 import './App.css'
+import MainComponent from './components/MainComponent'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      
+      <MainComponent />
     </>
   )
 }
---
+++ Reactive Restaurant/src/components/MainComponent.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Hrader from "./header/Header";
+import Body from "./body/Body";
+import Footer from "./footer/Footer";
+const MainComponent = () => {
+  return (
+    <>
+        <Hrader />
+        <Body />
+        <Footer />
+    </>
+  );
+}
+export default MainComponent;
