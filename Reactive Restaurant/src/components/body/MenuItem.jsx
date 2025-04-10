a45aebe091553dce220a780e9fd039dd0e8f06e9
sakibmahmodul <sakibuddessho89@gmail.com>
2025-04-10 16:16:18 +0600

    Designing Menu Item

--- Reactive Restaurant/src/components/body/MenuItem.jsx
+++ Reactive Restaurant/src/components/body/MenuItem.jsx
@@ -1,11 +1,18 @@
 import React from "react";
 import Menu from './Menu';
+import {Card, CardImg, CardImgOverlay, CardBody, CardTitle} from 'reactstrap';
 
 const MenuItem = props => {
-    console.log(props);
     return(
         <div>
-
+            <Card style={{margin: '10px'}}>
+                <CardBody>
+                    <CardImg width="100%" alt={props.dish.name} src={props.dish.image} style={{opacity: '0.5'}} />
+                    <CardImgOverlay>
+                        <CardTitle style={{textAlign: 'center'}}>{props.dish.name}</CardTitle>
+                    </CardImgOverlay>
+                </CardBody>
+            </Card>
         </div>
     )
 }
